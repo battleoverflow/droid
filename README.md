9eebc992f8580bf77ce4699e6f50849fe9ee76e9
Hifumi <hifumisecurity@protonmail.com>
2022-05-23 02:15:15 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# droid
-Droid is an Android communications script created to communicate with an Android device on the local network over the debug bridge
+# Droid
+Droid is an Android remote communications script created to communicate with an Android device on the local network over the Android debug bridge (adb).
+
+## Usage
+```bash
+python3 droid.py
+```
+
+## Options
+```
+-ip, --ip_address  |   IP address of the Android device
+-up, --upload      |   Name & location of the APK to upload (ex: ~/Downloads/ApkName.apk)
+-rm, --remove      |   Removes the old APK with the same package name
+-c,  --connect     |   Connects to the Android device
+-d,  --disconnect  |   Disconnects from the Android device
+-r,  --reboot      |   Remotely reboots the Android device
+-p,  --package     |   The package name of the APK (ex: com.android.ui)
+-i,  --info        |   Learn more about the Android device
+-dn,  --download   |   Download a file from the Android device
+```
+
+## Example(s)
+This example connects to the Android device (127.0.0.1 is an example), removes the specified APK package (`com.android.ui`), and then uploads a new APK called `test_apk_v1.apk`
+```bash
+python3 droid.py -ip 127.0.0.1 -c -rm -p com.android.ui -up ~/Downloads/test_apk_v1.apk
+```
+
+This example connects to the Android device (127.0.0.1 is an example), downloads an image, and then shows information about the Android device
+```bash
+python3 droid.py -ip 127.0.0.1 --download /sdcard/cool_pic.png -i
+```
+
+Once you're finished working within the Android environment, you can run this command to disconnect:
+```bash
+python3 droid.py -ip 127.0.0.1 -d
+```
---
+++ droid.py
@@ -0,0 +1,126 @@
+import os, argparse, sys, time, platform
+
+from subprocess import getoutput
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-ip', '--ip_address', help="IP address of the Android device", default=None, required=True)
+parser.add_argument('-up', '--upload', help="Name & location of the APK to upload (ex: ~/Downloads/ApkName.apk)", default=None, required=False)
+parser.add_argument('-rm', '--remove', help="Removes the old APK with the same package name", action='store_true', default=False, required=False)
+parser.add_argument('-c', '--connect', help="Connects to the Android device", action='store_true', default=False, required=False)
+parser.add_argument('-d', '--disconnect', help="Disconnects from the Android device", action='store_true', default=False, required=False)
+parser.add_argument('-r', '--reboot', help="Remotely reboots the Android device", action='store_true', default=False, required=False)
+parser.add_argument('-p', '--package', help="The package name of the APK (ex: com.android.ui)", default=None, required=False)
+parser.add_argument('-i', '--info', help="Learn more about the Android device", action='store_true', default=False, required=False)
+parser.add_argument('-dn', '--download', help="Download a file from the Android device", action='store_true', default=False, required=False)
+
+args = parser.parse_args()
+
+author = "Hifumi1337"
+version = "0.1.4"
+
+if platform.system() == 'Darwin':
+    adb = "$HOME/Library/Android/sdk/platform-tools/adb"
+elif platform.system() == 'Windows':
+    adb = "%LOCALAPPDATA%\Android\sdk\platform-tools/adb"
+elif platform.system() == 'Linux':
+    whoami = getoutput("whoami")
+    adb = f"/home/{whoami}/Android/Sdk/platform-tools/adb"
+
+class Droid:
+
+    def banner(self):
+        print(f"""
+        ·▄▄▄▄  ▄▄▄        ▪  ·▄▄▄▄  
+        ██· ██ ▀▄ █· ▄█▀▄ ██ ██· ██ 
+        ▐█▪ ▐█▌▐▀▀▄ ▐█▌.▐▌▐█·▐█▪ ▐█▌
+        ██. ██ ▐█•█▌▐█▌.▐▌▐█▌██. ██ 
+        ▀▀▀▀▀• .▀  ▀ ▀█▄▀▪▀▀▀▀▀▀▀▀• 
+            
+            {author} | v{version}
+        """)
+
+    def connect_ip(self):
+        try:
+            ip_address = args.ip_address
+            print("Connecting...")
+            time.sleep(0.2)
+            os.system(f'{adb} connect {ip_address}')
+        except:
+            print(f"Unable to connect to device {ip_address}")
+            time.sleep(0.5)
+            sys.exit(0)
+    
+    def disconnect_ip(self):
+        try:
+            ip_address = args.ip_address
+            print("Disconnecting...")
+            time.sleep(0.2)
+            os.system(f'{adb} disconnect {ip_address}')
+        except:
+            print(f"Unable to connect to device {ip_address}")
+            time.sleep(0.5)
+            sys.exit(0)
+
+    def remove_apk(self):
+        try:
+            package_name = args.package # Package Name
+            os.system(f'{adb} uninstall {package_name}')
+            print(f"{package_name} successfully removed")
+        except:
+            print(f"Unable to locate {package_name}")
+        
+    def upload_apk(self):
+        try:
+            name_of_apk = args.upload
+            os.system(f'{adb} install -r {name_of_apk}')
+            print(f"{name_of_apk} successfully uploaded")
+        except:
+            print(f"{name_of_apk} upload unsuccessful")
+    
+    if args.package == False and args.remove == True:
+        print("A package name is required to remove an APK")
+        sys.exit(0)
+    
+    def reboot(self):
+        try:
+            ip_address = args.ip_address
+            os.system(f"{adb} reboot {ip_address}")
+        except:
+            print("Device is unresponsive. Please check the connection")
+    
+    def info(self):
+        try:
+            ip_address = args.ip_address
+            os.system(f"{adb} usb")
+        except:
+            print("Unable to access Android device information")
+        
+    def download(self):
+        try:
+            file_location = args.download
+            os.system(f"{adb} pull {file_location}")
+        except:
+            print(f"Unable to locate {file_location}")
+
+if __name__ == '__main__':
+    D = Droid()
+    D.banner()
+
+    if args.connect:
+        D.connect_ip()
+
+        if args.remove and args.package:
+            D.remove_apk()
+    
+        if args.upload:
+            D.upload_apk()
+
+        if args.reboot:
+            D.reboot()
+        
+        if args.info:
+            D.info()
+    
+        if args.disconnect:
+            D.disconnect_ip()
